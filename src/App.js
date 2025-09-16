8ce54ea6c23784b246fabd16f228c4087faf019e
sofia <spardo1@ufl.edu>
2025-09-15 21:30:43 -0400

    Fix: resolved merge conflict in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import gatorLogo from './gator.gif';
+import logo from './gator.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Sofia and this is my project </p>
-=======
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! This is our team project.
+          Hello World! This is Sofia and this is our team project.
         </p>	
->>>>>>> 242861e (added new project text)
+
       </header>
     </div>
   );
